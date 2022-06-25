199c4365c18829d5384693f8a7c96bf404b73fe7
Keunmo <9858momo@gmail.com>
2022-06-25 23:05:44 +0900

    use localStorage and hide elements by checking localStorage value

--- app.js
+++ app.js
@@ -1,50 +1,28 @@
-// const loginForm = document.querySelector("#login-form");
-// const loginInput = document.querySelector("#login-form input");
-// const loginButton = document.querySelector("#login-form button");
-// console.log(loginInput);
-// console.log(loginButton);
-
-// function onLoginBtnClick() {
-//   // console.log("login btn clicked");
-//   // console.dir(loginInput);
-//   // console.log(loginInput.value);
-
-//   const username = loginInput.value;
-//   // if (username === "") {
-//   //   alert("input your name");
-//   // } else if(username.length > 15){
-//   //   alert("your username is too long");
-//   // } else {
-//   //   console.log(username);
-//   // }
-//   // 위의 입력 유효성 검사들은 html form으로 해결 가능.
-//   console.log(username);
-// }
-
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) { // 모든 EventListener의 첫번째 arg는 항상 event에 대한 infomation들이다.
   event.preventDefault(); // 브라우저의 기본 행동이 발생되지 않도록 막음. 원래 form은 자동으로 submit하는데 그걸 막는다던지.
-  // console.log(event);
-  const username = loginInput.value;
-  localStorage.setItem("username", username);
-  // console.log(username);
-  // greeting.innerText = "Hello " + username;
-  greeting.innerText = `Hello ${username}`; // same as upper line.
+  localStorage.setItem(USERNAME_KEY, loginInput.value);
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreetings();
+}
+
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
+  greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(event){
-  event.preventDefault();
-  console.dir(event);
-  // alert("clicked!");
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+  loginForm.classList.remove("hidden");
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings();
 }
-// loginButton.addEventListener("click", onLoginBtnClick);
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
